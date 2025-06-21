3a98fc0e209305ec3821a3bbead780c1f898198f
Thiago Berticelli Ló <losaum@gmail.com>
2025-06-21 03:43:33 -0300

    full_name movido par user e retirado dos perfins

--- src/application/dtos/add_nutritionist_profile_dto.py
+++ src/application/dtos/add_nutritionist_profile_dto.py
@@ -2,7 +2,6 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 class AddNutritionistProfileDTO(BaseModel):
-    full_name: str
     crn_number: str
     specialties: Optional[List[str]] = None
     certifications: Optional[List[str]] = None
